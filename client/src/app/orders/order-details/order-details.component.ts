93e28c8ace398f0a1bdc38aba4147c5c9637c225
jbtokam <jeanbTokam@github.com>
2025-03-31 21:30:13 -0400

    Added orders component

---
+++ client/src/app/orders/order-details/order-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-details',
+  standalone: false,
+  templateUrl: './order-details.component.html',
+  styleUrl: './order-details.component.css'
+})
+export class OrderDetailsComponent {
+
+}
---
+++ client/src/app/orders/orders.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { OrderFormComponent } from './order-form/order-form.component';
+import { OrderListComponent } from './order-list/order-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    OrderDetailsComponent,
+    OrderFormComponent,
+    OrderListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrdersModule { }
