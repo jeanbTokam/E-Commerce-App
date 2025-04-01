3e1bb4034b77108c1b1cd5d331aceebac145ebda
jbtokam <jeanbTokam@github.com>
2025-04-01 10:17:04 -0400

    Defined Cart Item

--- server/src/main/java/com/example/server/model/CartItem.java
+++ server/src/main/java/com/example/server/model/CartItem.java
@@ -1,4 +1,18 @@
 package com.example.server.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String productName;
+    private int quantity;
+    private Double price;
+
+    @ManyToOne
+    private Cart cart;
 }
