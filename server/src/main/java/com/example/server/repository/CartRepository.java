6f80d731654f6c089c67fed0b777b3bd867ca8cd
jbtokam <jeanbTokam@github.com>
2025-04-01 18:03:27 -0400

    Updated pom and app propertie and repository

---
+++ server/src/main/java/com/example/server/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.example.server.repository;
+
+import com.example.server.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
